9985bb3bf82c005ff9ec9c91162f758c7adac351
Donovan Fletcher <donovanfletcher29@gmail.com>
2024-05-22 23:40:53 -0400

    commiting again

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+const inquirer = require('inquirer');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  },
+  {
+    type: 'list',
+    name: 'color',
+    message: 'Select your favorite color:',
+    choices: ['Red', 'Blue', 'Green', 'Yellow']
+  },
+  {
+    type: 'checkbox',
+    name: 'toppings',
+    message: 'Select pizza toppings:',
+    choices: ['Pepperoni', 'Mushrooms', 'Onions', 'Sausage']
+  }
+];
+
+inquirer.prompt(questions)
+  .then(answers => {
+    console.log('User responses:', answers);
+  });
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+// function init() {}
+function App() {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>This is my React app.</p>
+        </div>
+    );
+}
 // Function call to initialize app
 init();
